9c9f9e69db7a24e8c2002b902477aa21843e85d3
Luis Diego <ldiegohcr@gmail.com>
2017-04-19 10:42:00 -0600

    Creacion pantalla plan estudios, modificacion a la clase horario

--- ProyectoDisenno/src/datos/Horario.java
+++ ProyectoDisenno/src/datos/Horario.java
@@ -1,17 +1,43 @@
 package datos;
 
 import java.sql.Time;
+import java.util.Date;
 
 public class Horario {
     
+    private Date PrimeraFecha;
+    private Date SegundaFecha;   
     private Time HoraInicio;
     private Time HoraFin;
+    
+    public Horario(Date PrimeraFecha, Date SegundaFecha, Time HoraInicio, Time HoraFin) {
+        this.PrimeraFecha = PrimeraFecha;
+        this.SegundaFecha = SegundaFecha;
+        this.HoraInicio = HoraInicio;
+        this.HoraFin = HoraFin;
+    }
 
     public Horario(Time HoraInicio, Time HoraFin) {
         this.HoraInicio = HoraInicio;
         this.HoraFin = HoraFin;
     }   
-    
+
+    public Date getPrimeraFecha() {
+        return PrimeraFecha;
+    }
+
+    public void setPrimeraFecha(Date PrimeraFecha) {
+        this.PrimeraFecha = PrimeraFecha;
+    }
+
+    public Date getSegundaFecha() {
+        return SegundaFecha;
+    }
+
+    public void setSegundaFecha(Date SegundaFecha) {
+        this.SegundaFecha = SegundaFecha;
+    }
+           
     public Time getHoraInicio() {
         return HoraInicio;
     }
