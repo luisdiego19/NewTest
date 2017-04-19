63e4fc7538bcd9a0c0dd9af5ba4895e0d1986735
Luis Diego <ldiegohcr@gmail.com>
2017-04-19 14:26:39 -0600

    Last Branch Before new Code

--- ProyectoDisenno/src/controlador/dto/DTOcarteraDocente.java
+++ ProyectoDisenno/src/controlador/dto/DTOcarteraDocente.java
@@ -3,6 +3,58 @@ package controlador.dto;
 
 public class DTOcarteraDocente 
 {
-    
-    
+    private String Identificacion;
+    private String Nombre;
+    private String Apellidos;
+    private String Correo;
+    private String Telefono; 
+
+    public DTOcarteraDocente(String Identificacion, String Nombre, String Apellidos, String Correo, String Telefono) {
+        this.Identificacion = Identificacion;
+        this.Nombre = Nombre;
+        this.Apellidos = Apellidos;
+        this.Correo = Correo;
+        this.Telefono = Telefono;
+    }
+
+    public String getIdentificacion() {
+        return Identificacion;
+    }
+
+    public void setIdentificacion(String Identificacion) {
+        this.Identificacion = Identificacion;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+            
 }
